588fe9b2961e74872f2709ed1a0575a5ee792e7d
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2021-07-16 21:39:30 +0530

    feat: timer

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const playButton = document.querySelector(".elements__play-btn");
 const pauseButton = document.querySelector(".elements__pause-btn");
 const audio = document.querySelector(".audio__file");
+let timeLeft = document.querySelector(".elements__time");
+
 
 playButton.addEventListener("click", () => {
     pauseButton.classList.add("show");
@@ -12,5 +14,17 @@ pauseButton.addEventListener("click", () => {
     audio.pause();
 })
 
-alert()
-alert()
+
+audio.addEventListener("timeupdate", function () {
+    let duration = parseInt(audio.duration);
+    let currentTime = parseInt(audio.currentTime);
+    timeLeft = duration - currentTime;
+
+    let s = timeLeft % 60;
+    let m = Math.floor(timeLeft / 60 ) % 60;
+
+    s = s < 10 ? "0"+s : s;
+    m = m < 10 ? "0"+m : m;
+
+    timeLeft.innerHTML = m+":"+s;
+}, false)
