c1405132198c6a2351f265476c2c62f6bc93a7e2
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2021-07-16 20:21:41 +0530

    feat: add audio play feature

---
+++ app/index.js
@@ -0,0 +1,13 @@
+const playButton = document.querySelector(".elements__play-btn");
+const pauseButton = document.querySelector(".elements__pause-btn");
+const audio = document.querySelector(".audio__file");
+
+playButton.addEventListener("click", () => {
+    pauseButton.classList.add("show");
+    audio.play();
+})
+
+pauseButton.addEventListener("click", () => {
+    pauseButton.classList.remove("show");
+    audio.pause();
+})
